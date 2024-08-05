5ba4fa6b96c389a3bf943947ca020e6b2caaf990
Daria Syusina <d.susina@rbt.ru>
2024-08-05 14:58:00 +0500

    sidebar

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { SvgComponent } from '../svg/svg.component';
+import { NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [SvgComponent, NgFor, RouterLink],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  menuItems = [
+    {
+      label: 'Моя страница',
+      icon: 'home',
+      link: '',
+    },
+    {
+      label: 'Чаты',
+      icon: 'chat',
+      link: '/chats',
+    },
+    {
+      label: 'Поиск',
+      icon: 'search',
+      link: '/search',
+    }
+  ];
+
 
 }
