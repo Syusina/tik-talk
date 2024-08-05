2b46f65e312724d783142a4febc4d843d1d79225
Daria Syusina <d.susina@rbt.ru>
2024-08-05 17:16:25 +0500

    subscriders

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,21 +1,21 @@
 import { Component, inject } from '@angular/core';
 import { SvgComponent } from '../svg/svg.component';
-import { AsyncPipe, NgFor } from '@angular/common';
+import { AsyncPipe, JsonPipe, NgFor } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { SubscriberCardComponent } from './subscriber-card/subscriber-card.component';
 import { ProfileService } from '../../data/services/profile.service';
-import { AsyncAction } from 'rxjs/internal/scheduler/AsyncAction';
 import { ImgUrlPipe } from '../../helpers/pipes/img-url.pipe';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SvgComponent, NgFor, RouterLink, SubscriberCardComponent, AsyncPipe, ImgUrlPipe],
+  imports: [SvgComponent, NgFor, RouterLink, SubscriberCardComponent, AsyncPipe, ImgUrlPipe, JsonPipe],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
   profileService = inject(ProfileService);
+
   subscribers$ = this.profileService.getSubscribersShortList();
 
   me = this.profileService.getMe();
